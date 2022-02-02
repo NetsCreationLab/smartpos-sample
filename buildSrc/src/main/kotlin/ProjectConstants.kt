5ecda935141b8bf2b623edcbdb34cd982cfbdc51
Nets Emerging Products <73833920+NetsCreationLab@users.noreply.github.com>
2022-02-02 10:35:13 +0100

    Update ProjectConstants.kt

--- buildSrc/src/main/kotlin/ProjectConstants.kt
+++ buildSrc/src/main/kotlin/ProjectConstants.kt
@@ -1,4 +1,4 @@
 object ProjectConstants {
     // Build constants that are used in build.gradle.kts for the project and application module
-    const val KOTLIN_VERSION: String = "1.5.21"
+    const val KOTLIN_VERSION: String = "1.6.10"
 }
